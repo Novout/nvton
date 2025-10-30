a0cf9ec95b7a5a9406cafc31ee3448df7721627b
Novout <novout@hotmail.com>
2025-10-29 21:24:19 -0300

    test: exclude nuxt files

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		coverage: {
 			provider: 'v8',
 			include: ['packages/**/src/**/*.ts'],
-			exclude: ['**/constants.ts', '**/types.ts', '**/index.ts'],
+			exclude: ['**/constants.ts', '**/types.ts', '**/index.ts', '**/nuxt/**'],
 		},
 	},
 });
